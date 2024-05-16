c424929c9f4dccde6a9abed0b668c8dcf2c25c21
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-05-16 22:36:33 +0530

    Initial Deploy

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import React from 'react'
+import { FaGithub as Github } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className=' bg-[#111113]'>
+        <div className=' flex items-center justify-between w-screen px-8 py-3 border-b border-gray-800 '>
+            <Link href={'/'}>
+                <h1 className=' font-bold text-[16px]'>Kairos</h1>
+            </Link>
+            <div className=' flex gap-4'>
+                <Link href={'https://github.com/ManojKumar2920/Kairos-Chatbot'} className=' flex border items-center justify-center gap-2 rounded-md p-2 border-gray-800'>
+                    <Github /><span className=' font-medium text-[12px] sm:hidden'>GitHub</span>
+                </Link>
+                <Link href={'auth/login'} className=' flex border items-center justify-center gap-2 rounded-md py-2 px-4 bg-[#fafafa] border-gray-800'>
+                    <span className=' font-bold text-[12px] text-[#111113]'>Login</span>
+                </Link>
+            </div>
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'sm': {'max': '639px'},
+        'md': {'max': '767px'},
+        'lg': {'max': '1023px'},
+        'xl': {'max': '1279px'},
+      },
     },
   },
   plugins: [],
