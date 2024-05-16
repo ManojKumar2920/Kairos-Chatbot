2b3f0200ed97245f9381aa7d2a7a23f9cccb64a4
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-05-16 21:14:09 +0530

    Initial Deploy

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar/navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Kairos AI",
+  description: "Kairos is the Generative AI build with Gemini Model",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        <div>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
