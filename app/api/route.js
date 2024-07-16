6847086ba51a189cf3baf638e3bd72f775fd8c29
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-07-16 13:28:54 +0530

    Production API Endpoint Errors

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,6 @@
-import axios from 'axios';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export const POST = async (req, res) => {
+export const POST = async (req,res) => {
   try {
     const body = await req.json();
     const { prompt } = body;
@@ -10,9 +9,7 @@ export const POST = async (req, res) => {
       return new Response(JSON.stringify({ error: 'Prompt is required' }), { status: 400 });
     }
 
-    const apiKey = process.env.GEMINI_API_KEY; // Accessing environment variable
-
-    const genAI = new GoogleGenerativeAI(apiKey);
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-flash-latest",
       systemInstruction: "kindly limit your response pertaining to topics such as web, development, programming, data, data structures and algorithm, analytics, nlp, ai and ml and more..if any other query received inform that your limitations. your name is Kairos AI. build with gemini lang model and next.js. developed by Manoj Kumar. his social profiles is https://www.linkedin.com/in/manojkumar20/ .the response provided by you need to be in great manner. only tell about yourself when they ask. don't tell that you're llm trained and developed by google. be act like matured and expert software engineer"
